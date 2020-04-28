9fbc110bcdd2a76ea23359269a3d976f938edd1d
Dan <dan96.austin@gmail.com>
2020-04-28 16:49:00 -0400

    Changes include color scheme beginning, add gallery image, set UK area, user entry fields for email name and number, radio buttons have replaced checkboxes

--- App/app/src/main/java/com/firstapp/helpapp/CreateLetter.java
+++ App/app/src/main/java/com/firstapp/helpapp/CreateLetter.java
@@ -1,19 +1,32 @@
 package com.firstapp.helpapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class CreateLetter extends AppCompatActivity {
 
+    Button addImageBut;
+    ImageView userImgView;
+    final int IMAGE_PICK_CODE = 10;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_letter_activity);
+
+        userImgView = findViewById(R.id.userImageView);
 
     }
 
@@ -22,8 +35,49 @@ public class CreateLetter extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void userInfoButton(View view) {
-        Intent intent = new Intent(this, UserInfo.class);
-        startActivity(intent);
+
+    public void addImageButton(View view) {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    IMAGE_PICK_CODE);
+        }
     }
+
+    public void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            userImgView.setImageURI(data.getData());
+            addImageBut.setText(R.string.change_image);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case IMAGE_PICK_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery();
+                } else {
+                    // permission denied.
+                    Toast.makeText(this, "Permission required to add a photo!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
 }
--- App/app/src/main/java/com/firstapp/helpapp/PreLetterCreation.java
+++ App/app/src/main/java/com/firstapp/helpapp/PreLetterCreation.java
@@ -1,22 +1,37 @@
 package com.firstapp.helpapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+
 
 public class PreLetterCreation extends AppCompatActivity {
 
-    Switch anonSwitch;
-    TextView usernameText, usernumText, anonDesc;
-    EditText usernameEdit, usernumEdit;
+    Switch anonSwitch, destinationSwitch;
+    Spinner destinationSpinner;
+    TextView recipientText, anonDesc;
+    EditText usernameEdit, emailEdit, numEdit;
+    RadioButton nhsCheck, elderlyCheck, gentlemanCheck, ladyCheck;
+    Boolean nhsChecked, ladyChecked;
+    ImageView personImage;
+    InputStream imageStream;
+    ViewGroup.LayoutParams params;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,38 +39,94 @@ public class PreLetterCreation extends AppCompatActivity {
         setContentView(R.layout.pre_letter_creation);
 
         usernameEdit = findViewById(R.id.userNameEdit);
-        usernumEdit = findViewById(R.id.userNumEdit);
-        usernameText = findViewById(R.id.userNameText);
-        usernumText = findViewById(R.id.userNumText);
-        anonDesc = findViewById(R.id.textAnon);
+        emailEdit = findViewById(R.id.emailEdit);
+        numEdit = findViewById(R.id.numEdit);
+        usernameEdit.setVisibility(View.INVISIBLE);
+        emailEdit.setVisibility(View.INVISIBLE);
+        numEdit.setVisibility(View.INVISIBLE);
 
+        personImage = findViewById(R.id.personImgView);
+        params = personImage.getLayoutParams();
+
+        destinationSpinner = findViewById(R.id.destinationSpinner);
+        destinationSwitch = findViewById(R.id.destinationSwitch);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.uk_areas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destinationSpinner.setAdapter(adapter);
+        if (!destinationSwitch.isChecked()) {destinationSpinner.setVisibility(View.GONE);}
+
+        destinationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {destinationSpinner.setVisibility(View.VISIBLE); destinationSwitch.setText(R.string.Yes);}
+                else {destinationSpinner.setVisibility(View.GONE); destinationSwitch.setText(R.string.No);}}});
+
+        anonDesc = findViewById(R.id.textAnon);
         anonSwitch = findViewById(R.id.switchAnon);
+        nhsCheck = findViewById(R.id.nhsCheckbox);
+        elderlyCheck = findViewById(R.id.elderlyCheckbox);
+        gentlemanCheck = findViewById(R.id.gentlemanCheckbox);
+        ladyCheck = findViewById(R.id.ladyCheckbox);
+        recipientText = findViewById(R.id.textRecipientDescr);
+        if (nhsCheck.isChecked()) {nhsChecked = true;} else {nhsChecked = false;}
+        if (ladyCheck.isChecked()) {ladyChecked = true;} else {ladyChecked = false;}
+
+        nhsCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {nhsChecked = true; recipientListener();}}});
+        elderlyCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {nhsChecked = false; recipientListener();}}});
+        ladyCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {ladyChecked = true; recipientListener();}}});
+        gentlemanCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {ladyChecked = false; recipientListener();}}});
+
         anonSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // Hide UI elements for user info
-                    usernameEdit.setVisibility(View.INVISIBLE);
-                    usernameText.setVisibility(View.INVISIBLE);
-                    usernumEdit.setVisibility(View.INVISIBLE);
-                    usernumText.setVisibility(View.INVISIBLE);
-                    anonSwitch.setText(R.string.Yes);
-                    anonDesc.setText(R.string.anon_expl);
-                } else {
-                    // Show UI elements for user info
                     usernameEdit.setVisibility(View.VISIBLE);
-                    usernameText.setVisibility(View.VISIBLE);
-                    usernumEdit.setVisibility(View.VISIBLE);
-                    usernumText.setVisibility(View.VISIBLE);
+                    emailEdit.setVisibility(View.VISIBLE);
+                    numEdit.setVisibility(View.VISIBLE);
                     anonSwitch.setText(R.string.No);
                     anonDesc.setText(R.string.non_anon_expl);
-                }
-            }
-        });
+                } else {
+                    // Show UI elements for user info
+                    usernameEdit.setVisibility(View.INVISIBLE);
+                    emailEdit.setVisibility(View.INVISIBLE);
+                    numEdit.setVisibility(View.INVISIBLE);
+                    anonSwitch.setText(R.string.Yes);
+                    anonDesc.setText(R.string.anon_expl);
+                    }}});
     }
 
     public void preContinueButton(View view) {
         Intent intent = new Intent(this, CreateLetter.class);
         startActivity(intent);
+    }
+
+    public void recipientListener() {
+        if (nhsChecked) {
+            if (ladyChecked) {recipientText.setText(R.string.nhs_female_explained);
+            imageStream = this.getResources().openRawResource(R.raw.nurse);}
+            else {recipientText.setText(R.string.nhs_male_explained);
+                imageStream = this.getResources().openRawResource(R.raw.doctor);}
+        } else {
+            if (ladyChecked) {recipientText.setText(R.string.elderly_female_explained);
+                imageStream = this.getResources().openRawResource(R.raw.grandma);}
+            else {
+            recipientText.setText(R.string.elderly_male_explained);
+                imageStream = this.getResources().openRawResource(R.raw.grandpa);}}
+        Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+        personImage.setImageBitmap(bitmap);
     }
 }
